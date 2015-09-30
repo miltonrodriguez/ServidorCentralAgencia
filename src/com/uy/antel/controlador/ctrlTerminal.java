4cfa6b5f245b49da9af5a0186d9a82e72a7f5ddd
miltonrodriguez <miltonrodriguez1985@gmail.com>
2015-09-30 19:00:19 -0300

    Subo solo lo importante

---
+++ src/com/uy/antel/controlador/ctrlTerminal.java
@@ -0,0 +1,43 @@
+package com.uy.antel.controlador;
+
+import java.net.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
+
+public class ctrlTerminal {
+
+	private static ctrlTerminal instance = null;
+
+	public static ctrlTerminal getInstance() {
+		if (instance == null) {
+			instance = new ctrlTerminal();
+		}
+		return instance;
+	}
+
+	
+	ServerSocket server;
+	public void listenSocket(){
+		  try{
+		    server = new ServerSocket(8082);
+		  } catch (IOException e) {
+		    System.out.println("Could not listen on port 8082");
+		    System.exit(-1);
+		  }
+		  while(true){
+		    atenderTerminal w;
+		    try{
+		//server.accept returns a client connection
+		      w = new atenderTerminal(server.accept());
+		      Thread t = new Thread(w);
+		      t.start();
+		    } catch (IOException e) {
+		      System.out.println("Accept failed: 8082");
+		      System.exit(-1);
+		    }
+		  }
+		}
+
+	
+
+}
